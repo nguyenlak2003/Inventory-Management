cbca910c604c8c76b776df32978f68dedb748320
nguyenlak2003 <nguyenlak1404@gmail.com>
2025-05-13 09:50:42 +0700

    base

--- inventory-software/app/page.js
+++ inventory-software/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <div>
         Thạch ngu như chó
       </div>
+      <Link href="/dashboard/home/dashboard">view dashboard</Link>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
